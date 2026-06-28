7a64cc19371032d27e8328bd61d45c240112ddf0
caveatashish <caveatashish@gmail.com>
2026-06-28 20:53:54 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZerodaTrade.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
